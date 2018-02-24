ce56993ec174e3f9b1391623a494fbbed64a3a81
Yaniv Carmel <cyanivde@gmail.com>
2018-02-24 14:31:07 +0200

    New questions.

--- questions.js
+++ questions.js
@@ -1,21 +1,38 @@
-var question = [{
-        question: "מה הטעם של חתול?",
-        answers: [
-            'פריך',
-            'עגול',
-            'מרופד',
-            'טעים'
+var question = [
+  {
+        q_question: "כמה זה 0 עצרת?",
+        q_answers: [
+            '1',
+            '0',
+            '-1/12',
+            'לא מוגדר'
         ],
-        done: False,
-        q_type: 'דעה'
+        q_type: 'classic',
+        q_category: undefined,
+        done: false
     },
     {
-        question: "מה הטעם של חתול?",
-        answers: [
-            'פריך',
-            'עגול',
-            'מרופד',
-            'טעים'
-        ],
-    }
+          q_question: "איזה איבר קיים רק אצל בני אדם?"
+          q_answers: [
+              'הסנטר',
+              'העקבים',
+              'בלוטות השקד',
+              'התוספתן'
+          ],
+          q_type: 'classic',
+          q_category: undefined,
+          done: false
+      },
+      {
+            q_question: "מה מהבאים אינו דבר אמיתי שיכול לקרות?"
+            q_answers: [
+                'אכילת יתר של גזר תצבע את עורך בכתום',
+                'אכילת יתר של חלזונות תצבע את עורך בצהוב',
+                'אכילת יתר של לובסטרים תצבע את עורך בוורוד',
+                'אכילת יתר של כסף תצבע את עורך בכסף'
+            ],
+            q_type: 'classic',
+            q_category: undefined,
+            done: false
+        },
 ];
