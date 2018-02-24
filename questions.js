957b95e1284a7c7758f9834feae770ce5cbc0509
Yaniv Carmel <cyanivde@gmail.com>
2018-02-24 15:15:05 +0200

    New questions.

--- questions.js
+++ questions.js
@@ -1,38 +1,146 @@
 var question = [
   {
-        q_question: "כמה זה 0 עצרת?",
-        q_answers: [
-            '1',
-            '0',
-            '-1/12',
-            'לא מוגדר'
-        ],
-        q_type: 'classic',
-        q_category: undefined,
-        done: false
-    },
-    {
-          q_question: "איזה איבר קיים רק אצל בני אדם?"
-          q_answers: [
-              'הסנטר',
-              'העקבים',
-              'בלוטות השקד',
-              'התוספתן'
-          ],
-          q_type: 'classic',
-          q_category: undefined,
-          done: false
-      },
-      {
-            q_question: "מה מהבאים אינו דבר אמיתי שיכול לקרות?"
-            q_answers: [
-                'אכילת יתר של גזר תצבע את עורך בכתום',
-                'אכילת יתר של חלזונות תצבע את עורך בצהוב',
-                'אכילת יתר של לובסטרים תצבע את עורך בוורוד',
-                'אכילת יתר של כסף תצבע את עורך בכסף'
-            ],
-            q_type: 'classic',
-            q_category: undefined,
-            done: false
-        },
+    q_question: 'כמה זה 0 עצרת?',
+    q_answers: [
+      '1',
+      '0',
+      '-1/12',
+      'לא מוגדר'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+    q_question: 'איזה איבר קיים רק אצל בני אדם?'
+    q_answers: [
+      'הסנטר',
+      'העקבים',
+      'בלוטות השקד',
+      'התוספתן'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+    q_question: 'מה מהבאים אינו דבר אמיתי שיכול לקרות?'
+    q_answers: [
+      'אכילת יתר של לובסטרים תצבע את עורך בוורוד',
+      'אכילת יתר של גזר תצבע את עורך בכתום',
+      'אכילת יתר של חלזונות תצבע את עורך בצהוב',
+      'אכילת יתר של כסף תצבע את עורך בכסף'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+    q_question: 'גילית שהכלב שלך אכל פיצה. איזו תוספת רעילה עבורו ותצדיק נסיעה לווטרינר?'
+    q_answers: [
+      'בצל',
+      'זית',
+      'אננס',
+      'תרד'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+    q_question: 'איזו מדינה לחמה לצד מדינות הציר במלחמת העולם השנייה?'
+    q_answers: [
+      'פינלנד',
+      'הרפובליקה הסינית',
+      'אוסטרליה',
+      'ניו זילנד'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+    q_question: 'איזה מהבאים לא קיים בגלגל המזלות הסיני?'
+    q_answers: [
+      'עיט',
+      'טיגריס',
+      'שור',
+      'ארנבת'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    done: false
+  },
+  {
+  q_question: 'מה מהבאים אינו ניקוד בג\'ודו?'
+  q_answers: [
+    'וואקמה',
+    'יוקו',
+    'ואזארי',
+    'איפון'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
+  {
+  q_question: 'חזרתי בזמן לתל אביב של 1990. איזה בניין יחסר לי בנוף?'
+  q_answers: [
+    'מגדלי עזראלי',
+    'תחנת הכוח רידינג',
+    'מגדל שלום',
+    'בניין העירייה'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
+  {
+  q_question: 'איזה משפט לא שימש כסיסמת בחירות?'
+  q_answers: [
+    'בנט - כיפת ברזל לעם ישראל',
+    'נתניהו - עושים שלום בטוח',
+    'רק ליברמן מבין ערבית',
+    'עלה ירוק או עלה לניידת'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
+  {
+  q_question: 'מהי המדינה הראשונה שבה ניתנה זכות הצבעה לנשים?'
+  q_answers: [
+    'ניו זילנד',
+    'ארצות הברית',
+    'ישראל',
+    'שוויץ'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
+  {
+  q_question: 'בתנ"ך, מה לא שימש ככלי הרג?'
+  q_answers: [
+    'כוס רעל',
+    'יתד של אוהל',
+    'לחי של חמור',
+    'עץ תלייה'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
+  {
+  q_question: 'איזה מהיסודות הוא מוליך החשמל הטוב ביותר?'
+  q_answers: [
+    'כסף',
+    'זהב',
+    'אלומיניום',
+    'נחושת'
+  ],
+  q_type: 'classic',
+  q_category: undefined,
+  done: false
+  },
 ];
