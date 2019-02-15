0f374f317852ed233d83705b5a04a773f506667f
Ron <ron23231@gmail.com>
2019-02-16 00:10:29 +0200

    added another set of questions

---
+++ questions - army version.js
@@ -0,0 +1,646 @@
+var questionsList = [
+  {
+    q_question: 'איזה מהבאים לא קיים בגלגל המזלות הסיני?',
+    q_answers: [
+      'עיט',
+      'טיגריס',
+      'שור',
+      'ארנבת'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'עיט',
+    done: false
+  },
+  {
+    q_question: 'באיזו שנה התגלתה האנטיביוטיקה הראשונה?',
+    q_answers: [
+      '1928',
+      '1948',
+      '1911',
+      '1961'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: '1928',
+    done: false
+  },
+  {
+    q_question: 'איזה מבין המלחמות והמבצעים האלו קרה קודם?',
+    q_answers: [
+      'מבצע אופרה',
+      'מבצע עמוד ענן',
+      'מלחמת סיני',
+      'מלחמת לבנון הראשונה'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'מלחמת סיני',
+    done: false
+  },
+  {
+    q_question: 'מה היתה שמה הראשוני של מלחמת לבנון הראשונה?',
+    q_answers: [
+      'מבצע שלום הגליל',
+      'מבצע כדור הרעם',
+      'מבצע נוף פראי',
+      'מבצע השבת השקט'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'מבצע שלום הגליל',
+    done: false
+  },
+  {
+    q_question: 'מי מבין הרמטכלים של צהל כיהן הכי הרבה זמן?',
+    q_answers: [
+      'רפאל איתן',
+      'משה דיין',
+      'משה יעלון',
+      'יעקוב דורי'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'רפאל איתן',
+    done: false
+  },
+  {
+    q_question: 'צבעי התג יחידה של המשטרה הצבאית הם?',
+    q_answers: [
+      'אדום כחול',
+      'כחול לבן',
+      'אדום לבן',
+      'ירוק צהוב'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'אדום כחול',
+    done: false
+  },
+  {
+    q_question: 'סמל תג היחידה של פיקוד דרום הוא דמוי?',
+    q_answers: [
+      'שועל',
+      'נחש',
+      'יעל',
+      'נמר'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'שועל',
+    done: false
+  },
+  {
+    q_question: 'אילו מבין ההבאים אינו חלק בנשק?',
+    q_answers: [
+      'שמורת ההדק',
+      'פין ציר גוף הנשק',
+      'טבעת חבק המתפסים',
+      'שביל הכוונת'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'שביל הכוונת',
+    done: false
+  },
+  {
+    q_question: 'דרגת טוראי ראשון (פס אחד) אינה קיימת עוד אך בעבר היא ניתנה ל?',
+    q_answers: [
+      'ישירות לאחר הטירונות',
+      'מצטיינים בטירונות ולנופלי קורס מ"כים',
+      'לטירונים שרק התחילו',
+      'הדרגה מעולם לא הייתה קיימת'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'מצטיינים בטירונות ולנופלי קורס מ"כים',
+    done: false
+  },
+  {
+    q_question: 'מאיזה חיל לא יצא רמטכל?',
+    q_answers: [
+      'חיל הים',
+      'חיל האוויר',
+      'חיל השיריון',
+      'חיל הרגלים'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'חיל הים',
+    done: false
+  },
+  {
+    q_question: 'מהו הסדר הנכון?',
+    q_answers: [
+      'ארמייה גיס אוגדה חטיבה גדוד פלוגה',
+      'גיס ארמייה אוגדה חטיבה גדוד פלוגה',
+      'ארמייה גיס אוגדה גדוד חטיבה פלוגה',
+      'גיס ארמייה גדוד אוגדה חטיבה פלוגה'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'ארמייה גיס אוגדה חטיבה גדוד פלוגה',
+    done: false
+  },
+  {
+    q_question: 'נלחמנו מול ערב הסעודית רק במלחמת?',
+    q_answers: [
+      'מלחמת העצמאות',
+      'מלחמת ששת הימים',
+      'מלחמת יום הכיפורים',
+      'מלחמת ההתשה'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'מלחמת העצמאות',
+    done: false
+  },
+  {
+    q_question: 'באיזו שנה נכתב מסמך רוח צהל?',
+    q_answers: [
+      '1994',
+      '1998',
+      '2001',
+      '2003'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: '1994',
+    done: false
+  },
+  {
+    q_question: 'מה היו מטרות המבצע קשת בענן?',
+    q_answers: [
+      'נטרול מחבלים, מניעת משלוח אמצעי מלחמה לסיני ואיתור מנהרות',
+      'מבצע רחב היקף של צהל בעזה בעקבות ירי כבד של רקטות',
+      'מבצע של חיל האוויר להשמיד מערך רקטות של חזבאללה',
+      'השבת חיילים חטופים'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'נטרול מחבלים, מניעת משלוח אמצעי מלחמה לסיני ואיתור מנהרות',
+    done: false
+  },
+  {
+    q_question: 'מה הם ראשי התיבות של סמל?',
+    q_answers: [
+      'סגן מחוץ למניין',
+      'סגן משנה לקצין',
+      'סגן משנה לסמר',
+      'סגן מפקד לבסיס'
+    ],
+    q_type: 'classic',
+    q_category: undefined,
+    correct_answer: 'סגן מחוץ למניין',
+    done: false
+  },
+  {
+    q_question: 'בתנ"ך, מה לא שימש ככלי הרג?',
+    q_answers: [
+      'כוס רעל',
+      'יתד של אוהל',
+      'לחי של חמור',
+      'עץ תלייה'
+    ],
+    q_type: 'category',
+    q_category: "בתנך הרגו הרבה",
+    correct_answer: 'כוס רעל',
+    done: false
+  },
+  {
+    q_question: 'איזה מהיסודות הוא מוליך החשמל הטוב ביותר?',
+    q_answers: [
+      'כסף',
+      'זהב',
+      'אלומיניום',
+      'נחושת'
+    ],
+    q_type: 'category',
+    q_category: "יסודות(לא, לא אווטר)",
+    correct_answer: 'כסף',
+    done: false
+  },
+  {
+    q_question: 'מה מהבאים לא נחשב "מכונה פשוטה"?',
+    q_answers: [
+      'בוכנה',
+      'גלגלת',
+      'בורג',
+      'יתד'
+    ],
+    q_type: 'category',
+    q_category: "מכונות",
+    correct_answer: 'בוכנה',
+    done: false
+  },
+  {
+    q_question: 'מי מהבאים לא כיהן כסגן ראש הממשלה?',
+    q_answers: [
+      'אהוד אולמרט',
+      'אביגדור ליברמן',
+      'אלי ישי',
+      'אהוד ברק'
+    ],
+    q_type: 'category',
+    q_category: "פוליטיקה",
+    correct_answer: 'אהוד אולמרט',
+    done: false
+  },
+  {
+    q_question: 'איזה מהבאים הוא לא מועד מקראי?',
+    q_answers: [
+      'חנוכה',
+      'יום הכיפורים',
+      'שבועות',
+      'ראש השנה'
+    ],
+    q_type: 'category',
+    q_category: "חגים ומועדים",
+    correct_answer: 'חנוכה',
+    done: false
+  },
+  {
+    q_question: 'מי מהבאים הופיע בישראל?',
+    q_answers: [
+      'ליידי גאגא',
+      'גורילז',
+      'דאפט פאנק',
+      'נירוונה'
+    ],
+    q_type: 'category',
+    q_category: "להקות ואמנים",
+    correct_answer: 'ליידי גאגא',
+    done: false
+  },
+  {
+    q_question: 'מתי נחתם הסכם השלום עם מצרים?',
+    q_answers: [
+      '1979',
+      '1971',
+      '1987',
+      '1991'
+    ],
+    q_type: 'category',
+    q_category: "שלום שלום",
+    correct_answer: '1979',
+    done: false
+  },
+  {
+    q_question: 'מהו אורכה של המדוזה הגדולה ביותר בעולם?',
+    q_answers: [
+      '30 מטר',
+      '15 מטר',
+      '2 מטר',
+      'חצי מטר'
+    ],
+    q_type: 'category',
+    q_category: "אמאאא, נעקצתי",
+    correct_answer: '30 מטר',
+    done: false
+  },
+  {
+    q_question: 'שטחה של אנגליה הוא?',
+    q_answers: [
+      '131 אלף קמ"ר',
+      '52 אלף קמ"ר',
+      '541 אלף קמ"ר',
+      '367 אלף קמ"ר'
+    ],
+    q_type: 'category',
+    q_category: "סימבה, כל האדמה הזו שלך",
+    correct_answer: '131 אלף קמ"ר',
+    done: false
+  },
+  {
+    q_question: 'מהו מפל המים הגבוה ביותר בעולם?',
+    q_answers: [
+      'מפלי אנחל בוונצואלה',
+      'מפלי הניאגרה',
+      'מפלי איגוסו בדרום אמריקה',
+      'מפלי ויקטוריה שעל נהר זמבזי'
+    ],
+    q_type: 'category',
+    q_category: "מי בא לראפטינג?",
+    correct_answer: 'מפלי אנחל בוונצואלה',
+    done: false
+  },
+  {
+    q_question: 'מי הלחין את היצירה "מפצח האגוזים"?',
+    q_answers: [
+      'צ\'ייקובסקי',
+      'בטהובן',
+      'ברהמס',
+      'פרוקופייב'
+    ],
+    q_type: 'category',
+    q_category: "סול סול-סול-סול לה לה סי סול לה",
+    correct_answer: 'צ\'ייקובסקי',
+    done: false
+  },
+  {
+    q_question: 'רבים מציוריו של קלוד מונה תיארו?',
+    q_answers: [
+      'נופים כפריים ועירוניים',
+      'רקדניות',
+      'חיי איכרים',
+      'חיות במנוסה'
+    ],
+    q_type: 'category',
+    q_category: "אומנות או נמות",
+    correct_answer: 'נופים כפריים ועירוניים',
+    done: false
+  },
+  {
+    q_question: 'איזה כישוף לוציוס מאלפוי עמד להטיל על הארי פוטר בסרט השני לפני שדובי עצר אותו?',
+    q_answers: [
+      'אבדה קדברה',
+      'קרושיו',
+      'אימפריו',
+      'סקטומסמפרה'
+    ],
+    q_type: 'category',
+    q_category: "פוטר!",
+    correct_answer: 'אבדה קדברה',
+    done: false
+  },
+  {
+    q_question: 'איפה סביר שתיתקל במונחים "דה קאפו", "ויואצ\'ה" ו"פרסטו"?',
+    q_answers: [
+      'בספר תווים של יצירה מוזיקלית',
+      'בספר בישול',
+      'ברשימה של אתלטים איטלקיים מפורסמים',
+      'בספר על ציירים איטלקיים'
+    ],
+    q_type: 'category',
+    q_category: "מה אתם מקשקשים פה?",
+    correct_answer: 'בספר תווים של יצירה מוזיקלית',
+    done: false
+  },
+  {
+    q_question: 'איזו מדינה לחמה לצד מדינות הציר במלחמת העולם השנייה?',
+    q_answers: [
+      'פינלנד',
+      'הרפובליקה הסינית',
+      'אוסטרליה',
+      'ניו זילנד'
+    ],
+    q_type: 'category',
+    q_category: "מלחה! מה?",
+    correct_answer: 'פינלנד',
+    done: false
+  },
+  {
+    q_question: 'אריאל שרון היה שר הבטחון ליותר ממלחמה אחת',
+    q_answers: [
+      'נכון',
+      'לא נכון'
+    ],
+    q_type: 'tf',
+    q_category: undefined,
+    correct_answer: 'לא נכון',
+    done: false
+  },
+  {
+    q_question: 'אצטרולב הוא כלי עבודה',
+    q_answers: [
+      'לא נכון',
+      'נכון'
+    ],
+    q_type: 'tf',
+    q_category: undefined,
+    correct_answer: 'לא נכון',
+    done: false
+  },
+  {
+    q_question: 'אם תשפוך מלח אל תוך כוס מים, גובה המים ירד במקום לעלות',
+    q_answers: [
+      'נכון',
+      'לא נכון'
+    ],
+    q_type: 'tf',
+    q_category: undefined,
+    correct_answer: 'נכון',
+    done: false
+  },
+  {
+    q_question: 'מה מהבאים אינו דבר אמיתי שיכול לקרות?',
+    q_answers: [
+      'אכילת יתר של לובסטרים תצבע את עורך בוורוד',
+      'אכילת יתר של גזר תצבע את עורך בכתום',
+      'אכילת יתר של חלזונות תצבע את עורך בצהוב',
+      'אכילת יתר של כסף תצבע את עורך בכסף'
+    ],
+    q_type: 'finals',
+    q_category: "הגמר",
+    correct_answer: 'אכילת יתר של לובסטרים תצבע את עורך בוורוד',
+    done: false
+  },
+  {
+    q_question: 'אם דיונון לא יפרק את האוכל שלו לפני שיבלע אותו, הוא יכול לגרום לעצמו נזק. למה?',
+    q_answers: [
+      'הוושט שלו יתרחב, מה שיגרום לנזק מוחי',
+      'הוושט שלו יתרחב ויחסום את הזימים שלו מבפנים',
+      'לאנזימים בקיבה שלו יקח יותר מדי זמן לפרק את המזון והוא יגווע ברעב',
+      'הקיבה שלו תתרחב ותפעיל לחץ על נוירונים קרובים'
+    ],
+    q_type: 'finals',
+    q_category: "הגמר",
+    correct_answer: 'הוושט שלו יתרחב, מה שיגרום לנזק מוחי',
+    done: false
+  },
+  {
+    q_question: 'מה נותן ליהלום כחול את הצבע שלו?',
+    q_answers: [
+      'חלק מאטומי הפחמן שלו הוחלפו באטומי בורון',
+      'יש יותר חנקן מהרגיל בסביבה כשהיהלום נוצר',
+      'ספיגת קרינה מהאדמה',
+      'תקופות קור קצרות במהלך היווצרותו ששינו את המבנה שלו'
+    ],
+    q_type: 'finals',
+    q_category: "הגמר",
+    correct_answer: 'חלק מאטומי הפחמן שלו הוחלפו באטומי בורון',
+    done: false
+  },
+  {
+    q_question: 'מה יונים, לעומת רוב שאר הציפורים, יכולות לעשות?',
+    q_answers: [
+      'למצוץ מים ולבלוע אותם',
+      'להימנע מנזק מקרינה קוסמית',
+      'לשרוד בלי מים למשך חודשים',
+      'להוציא את הצואה שלהן בכוח רב'
+    ],
+    q_type: 'finals',
+    q_category: "הגמר",
+    correct_answer: 'למצוץ מים ולבלוע אותם',
+    done: false
+  },
+  {
+    q_question: 'ב-1997 התרחש ניסיון ההתנקשות הכושל של המוסד ב...?',
+    q_answers: [
+      'ח\'אלד משעל',
+      'מוסא אבו מרזוק',
+      'אחמד יאסין',
+      'מוחמד דף'
+    ],
+    q_type: 'finals',
+    q_category: "הגמר",
+    correct_answer: 'ח\'אלד משעל',
+    done: false
+  },
+  {
+    q_question: 'כמה אחים ואחיות יש במשפחה של {} (כולל)?',
+    q_answers: [
+      '1',
+      '2',
+      '3',
+      '4+'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'לאן {} יבחר/תבחר לטייל מבין האפשרויות הבאות?',
+    q_answers: [
+      'מקסיקו',
+      'סין',
+      'צרפת',
+      'בוטסואנה'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'איך {} יעדיף/תעדיף לבלות?',
+    q_answers: [
+      'טיול בהרים',
+      'מסיבה',
+      'בינג\' של סדרה אהובה',
+      'משחק של charades'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'מה הכי חשוב ל{}?',
+    q_answers: [
+      'חברים',
+      'במשפחה',
+      'בריאות',
+      'Wi-Fi'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'מה העונה האהובה על {}?',
+    q_answers: [
+      'קיץ',
+      'חורף',
+      'סתיו',
+      'אביב'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'מתי בפעם האחרונה {} קרא ספר?',
+    q_answers: [
+      'ב-24 השעות האחרונות',
+      'בשבוע האחרון',
+      'בחודש האחרון',
+      'ספר? מה זה ספר?'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'מה {} היה לוקח איתו לאי בודד?',
+    q_answers: [
+      'ספר',
+      'מצ\'טה',
+      'סיר',
+      'סקייטבורד'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'עם מי {} מסתדר הכי טוב במשפחה שלו/שלה?',
+    q_answers: [
+      'אמא',
+      'אבא',
+      'אח (אם יש)',
+      'אחות (אם יש)'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'כמה ילדים יהיו ל{}...?',
+    q_answers: [
+      'לא',
+      '1-2',
+      '3-4',
+      '5+'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'לאיזה סוג סרט {} רוצה ללכת עכשיו?',
+    q_answers: [
+      'קומדיה רומנטית',
+      'גיבורי על',
+      'דוקומנטרי',
+      'אימה'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: 'אם {} היה בממשלה, הוא היה מגדיל את תקציב...',
+    q_answers: [
+      'החינוך',
+      'הרווחה',
+      'השיכון',
+      'הגנת הסביבה'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+  {
+    q_question: '{} השתחרר וקיבל מענק שחרור. הוא ישתמש בכסף בשביל...',
+    q_answers: [
+      'לקנות רכב',
+      'לשכור דירה',
+      'לממן טיול לחו"ל"',
+      'לא ישתמש אלא יחסוך'
+    ],
+    q_type: 'personal',
+    q_category: undefined,
+    correct_answer: undefined,
+    done: false
+  },
+];
